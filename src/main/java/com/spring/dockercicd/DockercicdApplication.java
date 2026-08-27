c80ff4bd913973b2004656e139f6d99c1fcd5d4e
hammad0757 <hammad0757>
2026-08-27 18:26:46 +0530

    docker ci cd

---
+++ src/main/java/com/spring/dockercicd/DockercicdApplication.java
@@ -0,0 +1,13 @@
+package com.spring.dockercicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockercicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockercicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/dockercicd/DockercicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.dockercicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockercicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
